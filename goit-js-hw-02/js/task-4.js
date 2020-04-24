0431f6e58d9d24552dd865104cbf45c1804e37ad
At0mUA <zatom89@gmail.com>
2020-04-24 17:59:23 +0300

    HW2 added task-4

---
+++ goit-js-hw-02/js/task-4.js
@@ -0,0 +1,28 @@
+const formatString = function(string) {
+  if (string.length > 40) {
+    let newString = '';
+    for (let i = 0; i <= 39; i += 1) {
+      newString += string[i];
+    }
+    return newString.concat('...');
+  }
+  return string;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
