02c6c09afcce1c9ab295b731b52000746cbda18b
At0mUA <zatom89@gmail.com>
2020-07-14 17:09:10 +0300

    added timer

---
+++ goit-js-hw-11/timer/script.js
@@ -0,0 +1,37 @@
+const refs = {
+  timer: document.querySelector('.timer'),
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+const targetDate = new Date('Jun 14, 2021');
+
+function timer() {
+  const currentTime = Date.now();
+  const timeLeft = targetDate - currentTime;
+  if (timeLeft < 0) {
+    return clearInterval(newTimer);
+  }
+  updateCountdownTimer(timeLeft);
+}
+
+const newTimer = setInterval(timer, 1000);
+
+function updateCountdownTimer(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
