7662d0c73286f0d4b2da48849ad315d7e7b12093
At0mUA <zatom89@gmail.com>
2020-07-29 11:11:41 +0300

    small fix

--- goit-js-hw-12/src/js/search-request.js
+++ goit-js-hw-12/src/js/search-request.js
@@ -4,10 +4,12 @@ import refs from './refs';
 export default function(value) {
   refs.countryList.innerHTML = '';
 
-  fetch(refs.url + value)
-    .then(res => res.json())
-    .then(data => {
-      countryListFilter(data, data.length);
-    })
-    .catch(error => console.log(error));
+  if (value.length > 0) {
+    return fetch(refs.url + value)
+      .then(res => res.json())
+      .then(data => {
+        countryListFilter(data, data.length);
+      })
+      .catch(error => console.log(error));
+  }
 }
